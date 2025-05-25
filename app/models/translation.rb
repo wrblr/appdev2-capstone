fc911dddd15b27eca14dd192a45ec5581ef620d8
wrblr <wrbell@uchicago.edu>
2025-05-25 19:09:52 +0000

    final initial db:migrate

--- app/models/translation.rb
+++ app/models/translation.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: translations
+#
+#  id          :bigint           not null, primary key
+#  body        :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  language_id :integer
+#  message_id  :integer
+#
 class Translation < ApplicationRecord
   belongs_to :message, required: true, class_name: "Message", foreign_key: "message_id"
   belongs_to :language, required: true, class_name: "Language", foreign_key: "language_id"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_25_190509) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_25_190631) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -186,6 +186,14 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_25_190509) do
     t.index ["expires_at"], name: "index_solid_queue_semaphores_on_expires_at"
     t.index ["key", "value"], name: "index_solid_queue_semaphores_on_key_and_value"
     t.index ["key"], name: "index_solid_queue_semaphores_on_key", unique: true
+  end
+
+  create_table "translations", force: :cascade do |t|
+    t.text "body"
+    t.integer "message_id"
+    t.integer "language_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
