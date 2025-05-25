cd330b39cd3968e50ef1f5ed44b43c49469fe2f8
wrblr <wrbell@uchicago.edu>
2025-05-25 19:06:14 +0000

    Scaffolded, migrated, and added model associations for Language

---
+++ app/models/language.rb
@@ -0,0 +1,14 @@
+# == Schema Information
+#
+# Table name: languages
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class Language < ApplicationRecord
+  has_many  :messages, class_name: "Message", foreign_key: "original_language_id", dependent: :destroy
+  has_many  :translations, class_name: "Translation", foreign_key: "language_id", dependent: :destroy
+  has_many  :users, class_name: "User", foreign_key: "preferred_language_id", dependent: :destroy
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_25_190325) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_25_190509) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -18,6 +18,12 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_25_190325) do
     t.string "name"
     t.string "image"
     t.integer "messages_count"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "languages", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
