f95126ab123d95dada350255e1365aeecee9fa4b
wrblr <wrbell@uchicago.edu>
2025-05-25 19:00:52 +0000

    added model associations for group and migrated

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,17 @@
+# == Schema Information
+#
+# Table name: groups
+#
+#  id             :bigint           not null, primary key
+#  image          :string
+#  messages_count :integer
+#  name           :string
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
 class Group < ApplicationRecord
+  has_many  :memberships, class_name: "Membership", foreign_key: "group_id", dependent: :destroy
+  has_many  :messages, class_name: "Message", foreign_key: "group_id", dependent: :nullify
+
+  has_many :users, through: :memberships, source: :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_24_235207) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_25_185746) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "groups", force: :cascade do |t|
+    t.string "name"
+    t.string "image"
+    t.integer "messages_count"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "solid_cable_messages", force: :cascade do |t|
     t.binary "channel", null: false
